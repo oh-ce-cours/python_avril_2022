59c66a26e17705b239d10b4bff0df72615befc3b
Matthieu Falce <matthieu@falce.net>
2022-04-26 17:49:14 +0200

    gitautosync update 25

--- tape_en_cours/exo_poo.py
+++ tape_en_cours/exo_poo.py
@@ -5,9 +5,11 @@ class Personne:
         self.naissance = naissance
         self.nom = nom
 
+    def get_age(self):
+        return 2020 - self.naissance
+
     def est_majeur(self) -> bool:
-        age = 2020 - self.naissance
-        return age > self.AGE_MAJORITE
+        return self.get_age() > self.AGE_MAJORITE
 
     def presentation(self):
-        return f"Je suis {self.nom}, j'ai {2020 - self.naissance} ans et je suis majeur {self.est_majeur}"
+        return f"Je suis {self.nom}, j'ai {self.get_age()} ans et je suis majeur {self.est_majeur}"
