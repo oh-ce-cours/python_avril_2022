7b1d717c528c5cd8b2fcc5cfd047ba9d1ff7902f
Matthieu Falce <matthieu@falce.net>
2022-04-26 11:40:26 +0200

    gitautosync update 1

--- tape_en_cours/test_fizz_buzz.py
+++ tape_en_cours/test_fizz_buzz.py
@@ -1,4 +1,4 @@
-from fizz_buzz import logique_fizz_buzz, est_divisible_par
+from fizz_buzz import logique_fizz_buzz, est_divisible_par, fizzbuzz_mieux_testable
 
 
 def test_fizz_buzz():
@@ -15,3 +15,24 @@ def test_fizz_buzz_avec_des_grands_nombres():
 def test_division():
     assert est_divisible_par(3, 3) == True
     assert est_divisible_par(5, 2) == False
+
+
+def test_integration_fizz_buzz():
+    assert fizzbuzz_mieux_testable(16) == [
+        "1",
+        "2",
+        "fizz",
+        "4",
+        "buzz",
+        "fizz",
+        "7",
+        "8",
+        "fizz",
+        "buzz",
+        "11",
+        "fizz",
+        "13",
+        "14",
+        "fizzbuzz",
+    ]
+    assert len(fizzbuzz_mieux_testable(5)) == 4
