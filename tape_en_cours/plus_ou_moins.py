70214dd36ecb018a13b5e6e0a43faa8e334e4f30
Matthieu Falce <matthieu@falce.net>
2022-04-26 12:29:03 +0200

    gitautosync update 9

--- tape_en_cours/plus_ou_moins.py
+++ tape_en_cours/plus_ou_moins.py
@@ -1,43 +1,43 @@
-import random
+# import random
 
-NOMBRE_A_TROUVER = 5
+# NOMBRE_A_TROUVER = 5
 
-# demande un nombre à l'utilisateur
-def demande_a_l_utilisateur():
-    return input("Entre un nombre s'il vous plait: ")
-
-
-def est_un_nombre(valeur: str) -> bool:
-    try:
-        float(valeur)
-    except ValueError:
-        return False
-    else:
-        return True
+# # demande un nombre à l'utilisateur
+# def demande_a_l_utilisateur():
+#     return input("Entre un nombre s'il vous plait: ")
 
 
-def demander_un_nombre_a_l_utilisateur() -> float:
-    entree = demande_a_l_utilisateur()
-    while not est_un_nombre(entree):
-        entree = demande_a_l_utilisateur()
-    return float(entree)
+# def est_un_nombre(valeur: str) -> bool:
+#     try:
+#         float(valeur)
+#     except ValueError:
+#         return False
+#     else:
+#         return True
 
 
-# essayer de convertir la chaine en nombre
-# indique si c'est plus ou moins
-# indique si c'est gagné
+# def demander_un_nombre_a_l_utilisateur() -> float:
+#     entree = demande_a_l_utilisateur()
+#     while not est_un_nombre(entree):
+#         entree = demande_a_l_utilisateur()
+#     return float(entree)
 
 
-def main():
-    while True:
-        nombre_propose = int(demander_un_nombre_a_l_utilisateur())
-        if nombre_propose > NOMBRE_A_TROUVER:
-            print("c'est moins")
-        elif nombre_propose < NOMBRE_A_TROUVER:
-            print("c'est plus")
-        else:
-            break
-    print("bravo")
+# # essayer de convertir la chaine en nombre
+# # indique si c'est plus ou moins
+# # indique si c'est gagné
+
+
+# def main():
+#     while True:
+#         nombre_propose = int(demander_un_nombre_a_l_utilisateur())
+#         if nombre_propose > NOMBRE_A_TROUVER:
+#             print("c'est moins")
+#         elif nombre_propose < NOMBRE_A_TROUVER:
+#             print("c'est plus")
+#         else:
+#             break
+#     print("bravo")
 
 
 if __name__ == "__main__":
