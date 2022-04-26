c5b9b55b7163d6144e09e5740f5cc5f29a705ce0
Matthieu Falce <matthieu@falce.net>
2022-04-26 12:05:21 +0200

    gitautosync update 16

--- tape_en_cours/plus_ou_moins.py
+++ tape_en_cours/plus_ou_moins.py
@@ -4,13 +4,17 @@ from tkinter.messagebox import NO
 NOMBRE_A_TROUVER = 5
 
 # demande un nombre à l'utilisateur
+def demande_a_l_utilisateur():
+    input("Entre un nombre s'il vous plait: ")
+
+
 # essayer de convertir la chaine en nombre
 # indique si c'est plus ou moins
 # indique si c'est gagné
 
 
 while True:
-    nombre_propose = int(input("Entre un nombre s'il vous plait: "))
+    nombre_propose = int()
     if nombre_propose > NOMBRE_A_TROUVER:
         print("c'est moins")
     elif nombre_propose < NOMBRE_A_TROUVER:
