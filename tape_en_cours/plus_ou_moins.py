d01a9c6a764cf117697e9b9ec3b0357eb18c6d7a
Matthieu Falce <matthieu@falce.net>
2022-04-26 12:00:25 +0200

    gitautosync update 7

--- tape_en_cours/plus_ou_moins.py
+++ tape_en_cours/plus_ou_moins.py
@@ -1,3 +1,10 @@
 import random
 
 NOMBRE_A_TROUVER = 5
+
+nombre_propose = input("Entre un nombre s'il vous plait: ")
+
+while True:
+    # demande un nombre à l'utilisateur 
+    # indique si c'est plus ou moins 
+    # indique si c'est gagné 
